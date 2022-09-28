3c92d5621799c796ac111fc014f3c0998c85702f
Dishant Nimavat <114412779+Dishant-Nimavat@users.noreply.github.com>
2022-09-28 10:05:41 +0530

    Refractored

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ def Greeting():
   print("Welcome to GitHub Actions! This is fantastic tool for CI/CD Integration")
   
 def Note():
-print("This is free open-source tool provides feature-reach functions for CI/CD")
+  print("This is free open-source tool provides feature-reach functions for CI/CD")
 
 print(Greeting())
