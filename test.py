c779afbdc402c410c16c972d8f9287291c8e13fa
Dishant Nimavat <114412779+Dishant-Nimavat@users.noreply.github.com>
2022-09-28 10:38:29 +0530

    Update_V2.0

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 def Greeting():
     print("Welcome to GitHub Actions! This is fantastic tool for CI/CD Integration")
     
-  
+    
 def Note():
     print("This is free open-source tool provides feature-reach functions for CI/CD")
 
+    
 print(Greeting())
